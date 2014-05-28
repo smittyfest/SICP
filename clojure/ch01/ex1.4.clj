a045519e5961909784dea1db8089ee70bb486958
Eric Smith <esmith@thebrighttag.com>
2014-05-27 20:01:06 -0500

    Added comment to ex1.4.clj

--- clojure/ch01/ex1.4.clj
+++ clojure/ch01/ex1.4.clj
@@ -2,6 +2,7 @@
 
 (defn a-plus-abs-b [a b] ((if (> b 0) + -) a b))
 
+;; should add the absolute value of b to a
 (deftest test-a-plus-abs-b
   (is (= 9 (a-plus-abs-b 4,  5)))
   (is (= 9 (a-plus-abs-b 4, -5)))
