09a9ea4e69311748819014861b4df7e41d4154ae
smittyfest <smittyfest@users.noreply.github.com>
2014-05-31 12:00:56 -0500

    Update ex1.7.scm
    messing with formatting

--- scheme/ch01/ex1.7.scm
+++ scheme/ch01/ex1.7.scm
@@ -5,22 +5,18 @@
 
 (define (square x) (* x x))
 
-(define (average x y)
-  (/ (+ x y) 2))
+(define (average x y) (/ (+ x y) 2))
     
-(define (improve guess x)
-  (average guess (/ x guess)))
+(define (improve guess x) (average guess (/ x guess)))
         
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.00001))
+(define (good-enough? guess x) (< (abs (- (square guess) x)) 0.00001))
 	    
 (define (sqrt-iter guess x)
   (if (good-enough? guess x)
     guess
     (sqrt-iter (improve guess x) x)))
 
-(define (sqrt x)
-  (sqrt-iter 1.0 x))
+(define (sqrt x) (sqrt-iter 1.0 x))
 			  
 ; Refined implementation
 
@@ -32,8 +28,7 @@
     guess
     (sqrt-iter2 (improve guess x) guess x)))
 
-(define (sqrt2 x)
-  (sqrt-iter2 1.0 0.0 x))
+(define (sqrt2 x) (sqrt-iter2 1.0 0.0 x))
 
 ; The original sqrt is actually more accurate for large X, if we use a larger
 ; delta (0.001) in the good-enough? and good-enough2? methods. With 0.00001, 
