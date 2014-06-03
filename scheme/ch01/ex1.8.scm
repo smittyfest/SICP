b216c7a354c4ee853cf5b26493e4d5cdaf0c08ad
Eric Smith <esmith@thebrighttag.com>
2014-06-02 22:25:54 -0500

    Added some scheme functions for ex1.8

--- scheme/ch01/ex1.8.scm
+++ scheme/ch01/ex1.8.scm
@@ -12,3 +12,15 @@
 ;; root procedure.
 #lang scheme
 (require (planet schematics/schemeunit:3:5))
+
+(define (cube x) (* x x x))
+
+(define (abs x) (cond (neg? x) (- x) x))
+
+(define (improve guess x) (/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
+
+(define (good-enough? guess x) (< (abs (- (/ (cube guess) x) 1.0)) 0.0001))
+
+(define (cube-root-iter guess x) (if (good-enough? guess x) guess (cube-root-iter (improve guess x) x)))
+
+(define (cube-root x) (cube-root-iter 1.0 x))
