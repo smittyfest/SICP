b9683936049fa099bcbf055c3a2d3afc456c8c9f
Eric Smith <esmith@thebrighttag.com>
2014-06-02 22:11:55 -0500

    Added cube, abs, and improve functions to Clojure 1.8 solution

--- clojure/ch01/ex1.8.clj
+++ clojure/ch01/ex1.8.clj
@@ -1,2 +1,20 @@
+;;
+;; Exercise 1.8
+;;
+;; Newton's method for cube roots is based on the fact that if y is an approximation
+;; to the cube root of x, then a better approximation is given by the value
+;;
+;; (x/y^2) + 2y
+;; ============
+;;      3
+;;
+;; Use this formula to implement a cube root procedure analogous to the square root
+;; procedure.
+;;
 (ns sicp.ch01 (:use clojure.test))
 
+(defn cube [x] (* x x x))
+
+(defn abs [x] (cond (neg? x) (- x) :else x))
+
+(defn improve [guess x] (/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
