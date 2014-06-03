3d362093914683848973bdc7831228542ce4fa39
Eric Smith <esmith@thebrighttag.com>
2014-06-02 22:21:29 -0500

    Added more functions

--- clojure/ch01/ex1.8.clj
+++ clojure/ch01/ex1.8.clj
@@ -18,3 +18,9 @@
 (defn abs [x] (cond (neg? x) (- x) :else x))
 
 (defn improve [guess x] (/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
+
+(defn good-enough? [guess x] (< (abs (- (/ (cube guess) x) 1.0)) 0.0001))
+
+(defn cube-root-iter [guess x] (if (good-enough? guess x) guess (cube-root-iter (improve guess x) x)))
+
+(defn cube-root [x] (cube-root-iter 1.0 x))
