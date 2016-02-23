36552695c594b2dc2e2826c76732f302180cbc29
smittyfest <smittyfest@users.noreply.github.com>
2016-02-23 15:26:14 -0600

    Update ex1.14.clj

--- clojure/ch01/ex1.14.clj
+++ clojure/ch01/ex1.14.clj
@@ -1,8 +1,11 @@
 (ns sicp.ch01 (:use clojure.test))
 
-(defn count-change [money, coins] 
-  (cond (or (empty? coins) (neg? money)) 0
-  (zero? money) 1 
+(defn count-change
+  {:doc "Returns the number of ways to count change for a given amount with a given set of coins"}
+  [money, coins] 
+  (cond 
+    (or (empty? coins) (neg? money)) 0
+    (zero? money) 1 
     :else (+ 
       (count-change money (rest coins))
       (count-change (- money (first coins)) coins)
