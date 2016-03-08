801780181f4ee4a9a552a22d34e93832c38abe51
smittyfest <smittyfest@users.noreply.github.com>
2016-03-07 20:23:14 -0600

    Create Exercise-1-01.scala

---
+++ scala/chapter1/Exercise-1-01.scala
@@ -0,0 +1,27 @@
+Scala version 2.11.6
+scala> 10
+res0: Int = 10
+
+scala> 5 + 3 + 4
+res1: Int = 12
+
+scala> 9 - 1
+res2: Int = 8
+
+scala> 6 / 2
+res3: Int = 3
+
+scala> (4 * 2) + (4 - 6)
+res5: Int = 6
+
+scala> val a = 3
+a: Int = 3
+
+scala> val b = a + 1
+b: Int = 4
+
+scala> a + b + (a * b)
+res6: Int = 19
+
+scala> a == b
+res7: Boolean = false
