32c6673d8bbc5549d74759256c2915f8a7c70ee2
smittyfest <smittyfest@users.noreply.github.com>
2016-03-11 15:59:59 -0600

    Update exercise-1.16.clj

--- clojure/chapter1/exercise-1.16.clj
+++ clojure/chapter1/exercise-1.16.clj
@@ -1,1 +1,11 @@
 (ns sicp.ch01 (:use clojure.test))
+
+(defn fast-expt
+  [b n]
+  (defn fast-iter
+    [b counter accu]
+    (cond
+      (= counter 0) accu
+      (even? counter) (fast-iter (square b) (/ counter 2) accu)
+      :else (fast-iter b (- counter 1) (* accu b))))
+    (fast-iter b n 1))
