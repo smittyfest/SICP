3ce5bf7ec86e795cc07252a29237b59b2e0319d3
smittyfest <smittyfest@users.noreply.github.com>
2016-03-17 18:52:25 -0500

    Update exercise-1.04.clj

--- clojure/chapter1/exercise-1.04.clj
+++ clojure/chapter1/exercise-1.04.clj
@@ -1,8 +1,10 @@
 (ns sicp.ch01 (:use clojure.test))
 
-(defn a-plus-abs-b [a b] ((if (> b 0) + -) a b))
+;; this procedure should add the absolute value of b to a
+(defn a-plus-abs-b
+  [a b]
+  ((if (> b 0) + -) a b))
 
-;; should add the absolute value of b to a
 (deftest test-a-plus-abs-b
   (is (= 9 (a-plus-abs-b 4,  5)))
   (is (= 9 (a-plus-abs-b 4, -5)))
