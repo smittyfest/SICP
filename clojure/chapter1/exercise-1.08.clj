c9d13a87d6b4f19a67c8e636c0a348b2156905c3
smittyfest <smittyfest@users.noreply.github.com>
2016-03-17 19:14:23 -0500

    Update exercise-1.08.clj

--- clojure/chapter1/exercise-1.08.clj
+++ clojure/chapter1/exercise-1.08.clj
@@ -13,14 +13,31 @@
 ;;
 (ns sicp.ch01 (:use clojure.test))
 
-(defn cube [x] (* x x x))
+(defn cube
+  [x] (* x x x))
 
-(defn abs [x] (cond (neg? x) (- x) :else x))
+(defn abs
+  [x]
+  (cond
+    (neg? x) (- x)
+    :else x))
 
-(defn improve [guess x] (/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
+(defn improve 
+  [guess x]
+  (/ (+ (/ x (* guess guess))
+        (* 2 guess)) 3))
 
-(defn good-enough? [guess x] (< (abs (- (/ (cube guess) x) 1.0)) 0.0001))
+(defn good-enough?
+  [guess x]
+  (< (abs (- (/ (cube guess) x) 1.0)) 0.0001))
 
-(defn cube-root-iter [guess x] (if (good-enough? guess x) guess (cube-root-iter (improve guess x) x)))
+(defn cube-root-iter
+  [guess x]
+  (if (good-enough? guess x) guess
+    (cube-root-iter (improve guess x) x)))
 
-(defn cube-root [x] (cube-root-iter 1.0 x))
+(defn cube-root
+  [x]
+  (cube-root-iter 1.0 x))
+
+;; unit tests
