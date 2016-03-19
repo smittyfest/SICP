6606d40067702989ef58007f4557d5402f4319a0
smittyfest <smittyfest@users.noreply.github.com>
2016-03-18 21:56:20 -0500

    Update exercise-1.19.clj

--- clojure/chapter1/exercise-1.19.clj
+++ clojure/chapter1/exercise-1.19.clj
@@ -6,3 +6,6 @@
 ;; the fib-iter process of section 1.2.2: (a <- a+b) and (b <- a). Call this transformation 'T',
 ;; and observe that applying 'T' over and over again 'n' times, starting with 1 and 0, produces
 ;; the pair Fib(n+1) and Fib(n).
+;; In other words, the Fibonacci numbers are produced by applying T^n, the nth power of the
+;; transformation T, starting with the pair (1,0). Now consider T to be the special case of p = 0 and
+;; q = 1 in a family of transformations Tpq, where Tpq transforms the pair (a, b)
