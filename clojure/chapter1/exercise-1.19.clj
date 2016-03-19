01aed77858952233f5fdf23e0e75a0c2d3561b2d
smittyfest <smittyfest@users.noreply.github.com>
2016-03-18 21:52:51 -0500

    Update exercise-1.19.clj

--- clojure/chapter1/exercise-1.19.clj
+++ clojure/chapter1/exercise-1.19.clj
@@ -2,4 +2,7 @@
 ;; Exercise-1.19
 ;;
 ;; There is a clever algorithm for computing the Fibonacci numbers in a logarithmic
-;; number of steps.
+;; number of steps. Recall the transformation of the state variables 'a' and 'b' in
+;; the fib-iter process of section 1.2.2: (a <- a+b) and (b <- a). Call this transformation 'T',
+;; and observe that applying 'T' over and over again 'n' times, starting with 1 and 0, produces
+;; the pair Fib(n+1) and Fib(n).
