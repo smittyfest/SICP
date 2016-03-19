282f7c5438931642788044393a243eac0d7bf4dd
smittyfest <smittyfest@users.noreply.github.com>
2016-03-18 22:08:06 -0500

    Update exercise-1.19.clj

--- clojure/chapter1/exercise-1.19.clj
+++ clojure/chapter1/exercise-1.19.clj
@@ -12,3 +12,26 @@
 ;; (a <- bq + aq + ap) and (b <- bp + aq). Show that if we apply such a transformation Tpq twice,
 ;; the effect is the same as using a single transformation Tp'q' of the same form, and compute p' and q'
 ;; in terms of p and q.
+;;
+;; This gives us an explicit way to square these transformations, and thus we can compute T^n using
+;; successive squaring, as in the fast-expt procedure. Put this all together to complete the following
+;; procedure, which runs in a logarithmic number of steps:
+;;
+;; (define (fib n)
+;;   (fib-iter 1 0 0 1 n))
+;;
+;; (define (fib-iter a b p q count)
+;;   (cond ((= count 0) b)
+;;         ((even? count)
+;;          (fib-iter a
+;;                    b
+;;                    (??)    ; compute p'
+;;                    (??)    ; compute q'
+;;                    (/ count 2)))
+;;         (else (fib-iter (+ (* b q) (* a q) (* a p))
+;;                         (+ (* b p) (* a q))
+;;                         p
+;;                         q
+;;                         (- count 1)))))
+;;
+(ns sicp.ch01 (:use clojure.test))
