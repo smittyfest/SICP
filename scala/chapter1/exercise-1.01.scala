6c0084336c8311cc5293750f44dd387b5257ba60
smittyfest <smittyfest@users.noreply.github.com>
2016-03-28 12:53:13 -0500

    Update exercise-1.01.scala

--- scala/chapter1/exercise-1.01.scala
+++ scala/chapter1/exercise-1.01.scala
@@ -12,7 +12,7 @@ scala> 6 / 2
 res3: Int = 3
 
 scala> (4 * 2) + (4 - 6)
-res5: Int = 6
+res4: Int = 6
 
 scala> val a = 3
 a: Int = 3
@@ -21,13 +21,17 @@ scala> val b = a + 1
 b: Int = 4
 
 scala> a + b + (a * b)
-res6: Int = 19
+res5: Int = 19
 
 scala> a == b
-res7: Boolean = false
+res6: Boolean = false
 
 scala> if ((b > a) && (b < (a * b))) b else a
-res5: Int = 4
+res7: Int = 4
 
 scala> if (a == 4) 6 else if (b == 4) (6 + 7 + a) else 25
-res6: Int = 16
+res8: Int = 16
+
+scala> 2 + (if (b > a) b else a)
+res9: Int = 6
+
