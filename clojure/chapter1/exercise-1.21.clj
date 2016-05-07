45e719824d52487d495aa4d0d5258375ad19c633
smittyfest <smittyfest@users.noreply.github.com>
2016-05-07 13:05:39 -0500

    Update exercise-1.21.clj

--- clojure/chapter1/exercise-1.21.clj
+++ clojure/chapter1/exercise-1.21.clj
@@ -11,10 +11,12 @@
   [test-divisor n]
   (= (rem n test-divisor) 0))
 
-(defn find-divisor [n test-divisor]
-  (cond (> (square test-divisor) n) n
-        (divides? test-divisor n) test-divisor
-        :else (find-divisor n  (+ test-divisor 1))))
+(defn find-divisor
+  [n test-divisor]
+  (cond 
+    (> (square test-divisor) n) n
+    (divides? test-divisor n) test-divisor
+    :else (find-divisor n  (+ test-divisor 1))))
 
 (defn smallest-divisor [n] (find-divisor n 2))
 
